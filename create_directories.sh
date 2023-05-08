ada02aa508a6d643a1774eec32754f9800ca057d
Michaël Silvestre <willtard@gmail.com>
2023-05-09 01:05:37 +0200

    feature(data): add data default config

--- create_directories.sh
+++ create_directories.sh
@@ -1,5 +1,5 @@
 !#/bin/zsh
 
-sudo mkdir -p /data/homeassistant/config
+sudo mkdir -p /data/homeassistant
 sudo mkdir -p /data/mqtt/data /data/mqtt/log /data/mqtt/config
 sudo mkdir -p /data/zigbee2mqtt
